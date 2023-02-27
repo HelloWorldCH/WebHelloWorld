15b1cf7956b07fb4bcbc717248cd462e5605ab32
HelloWorldCH <oaklaza003@gmail.com>
2023-02-28 03:05:59 +0700

    Upload CSS install & Javascript install finish
    Free file css & javascript to install website setup database now finish

--- javascript/install.js
+++ javascript/install.js
@@ -1,8 +1,3 @@
-// โค้ดนี้ถูกสร้างขึ้นโดย HelloWorld
-// และอนุญาติให้มีการนำไปใช้เพื่อการศึกษาและการเรียนรู้เท่านั้น
-// ไม่อนุญาตให้นำโค้ดนี้ไปใช้ในเชิงพาณิชย์หรือจำหน่ายโดยเด็ดขาด
-// และไม่รับประกันความเหมาะสมหรือความเสียหายที่อาจเกิดขึ้นจากการใช้งานไฟล์นี้ในกรณีใดๆ ทั้งสิ้น
-
 if (window.location.search === '?step=1') {
   const boxCheck = document.querySelector('.hw-box-check');
   const boxCheckSupport = boxCheck.querySelectorAll('.hw-box-check-support');
@@ -31,23 +26,30 @@ if (window.location.search === '?step=2') {
   const dbPassword = document.getElementById('db-password');
   const dbName = document.getElementById('db-name');
 
-  const btnInstall = document.getElementById('hw-button-install-id');
+  const btnInstall = document.getElementById('hw-button-install-id-database');
 
   // สร้าง function สำหรับตรวจสอบฟิลด์ทุกตัว
   function validateFields() {
       if (!dbHost.value || !dbPort.value || !dbUsername.value || !dbName.value) {
           btnInstall.classList.add('hw-button-install-disabled');
-          btnInstall.setAttribute('disabled', 'disabled'); // เพิ่ม attribute disabled
           btnInstall.title = 'กรุณาตรวจสอบข้อมูลให้ครบถ้วน';
           btnInstall.addEventListener('click', function (e) {
             e.preventDefault();
             modal_wrapper.classList.add("active");
             s_modal.classList.remove("active");
             e_modal.classList.add("active");
+            window.location.href = '#';
           });
       } else {
           btnInstall.classList.remove('hw-button-install-disabled');
-          btnInstall.removeAttribute('disabled'); // ลบ attribute disabled
+          btnInstall.title = 'พร้อมติดตั้ง';
+          btnInstall.addEventListener('click', function (e) {
+            e.preventDefault();
+            modal_wrapper.classList.remove("active");
+            s_modal.classList.remove("active");
+            e_modal.classList.remove("active");
+            window.location.href = this.getAttribute('href');
+          });
       }
   }
 
@@ -57,17 +59,7 @@ if (window.location.search === '?step=2') {
   dbUsername.addEventListener('input', validateFields);
   dbName.addEventListener('input', validateFields);
 
-}
-
-if (window.location.search === '?step=2') {
-  const btnInstallDatabase = document.getElementById('hw-button-install-id-database');
-  btnInstallDatabase.addEventListener("click", () => {
-    
-    const dbHost = document.getElementById('db-host').value;
-    const dbPort = document.getElementById('db-port').value;
-    const dbUsername = document.getElementById('db-username').value;
-    const dbPassword = document.getElementById('db-password').value;
-    const dbName = document.getElementById('db-name').value;
+  btnInstall.addEventListener("click", () => {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -76,8 +68,63 @@ if (window.location.search === '?step=2') {
     };
     xhttp.open('POST', 'function.php?step=createdatabase', true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send(`dbHost=${dbHost} &dbPort=${dbPort} &dbUsername=${dbUsername} &dbPassword=${dbPassword} &dbName=${dbName}`);
+    xhttp.send(`dbHost=${dbHost.value} &dbPort=${dbPort.value} &dbUsername=${dbUsername.value} &dbPassword=${dbPassword.value} &dbName=${dbName.value}`);
   });
+
+}
+
+if (window.location.search === '?step=3') {
+  const btnWebSetup = document.getElementById('hw-button-install-id-websetup');
+  const webname = document.getElementById('web-name');
+  const webuseradmin = document.getElementById('web-username-admin');
+  const webpassadmin = document.getElementById('web-password-admin');
+  const webconfirmpassadmin = document.getElementById('web-confirm-password-admin');
+  validateFields()
+
+  // สร้าง function สำหรับตรวจสอบฟิลด์ทุกตัว
+  function validateFields() {
+    if (!webname.value || !webuseradmin.value || !webpassadmin.value || !webconfirmpassadmin.value || (webpassadmin.value !== webconfirmpassadmin.value)) {
+        btnWebSetup.classList.add('hw-button-install-disabled');
+        btnWebSetup.title = 'กรุณาตรวจสอบข้อมูลให้ครบถ้วน';
+        btnWebSetup.addEventListener('click', function (e) {
+          e.preventDefault();
+          modal_wrapper.classList.add("active");
+          s_modal.classList.remove("active");
+          e_modal.classList.add("active");
+          window.location.href = '#';
+        });
+        if (webpassadmin.value !== webconfirmpassadmin.value) {
+          btnWebSetup.value = 'รหัสผ่านไม่ตรงกัน';
+        }
+    } else {
+        btnWebSetup.classList.remove('hw-button-install-disabled');
+        btnWebSetup.title = 'พร้อมติดตั้ง';
+        btnWebSetup.addEventListener('click', function (e) {
+          e.preventDefault();
+          modal_wrapper.classList.remove("active");
+          s_modal.classList.remove("active");
+          e_modal.classList.remove("active");
+          window.location.href = this.getAttribute('href');
+        });      
+    }
+  }
+
+  btnWebSetup.addEventListener("click", () => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+      }
+    };
+    xhttp.open('POST', 'function.php?step=setupweb', true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send(`webname=${webname.value} &webuseradmin=${webuseradmin.value} &webpassadmin=${webpassadmin.value} &webconfirmpassadmin=${webconfirmpassadmin.value}`);
+  });
+
+  webname.addEventListener('input', validateFields);
+  webuseradmin.addEventListener('input', validateFields);
+  webpassadmin.addEventListener('input', validateFields);
+  webconfirmpassadmin.addEventListener('input', validateFields);
 }
 
 var close_btns = document.querySelectorAll(".close_btn");
